c397cefa7fe245e101ba74a2f64c414827312244
techintosh12 <frankyding12@gmail.com>
2017-02-03 13:13:11 -0500

    Setting stuff up for ListView to display rides
    - setting up the most important stuff

--- RideTJ2/app/src/main/java/com/frankding/android/ridetj/catchrideActivity.java
+++ RideTJ2/app/src/main/java/com/frankding/android/ridetj/catchrideActivity.java
@@ -2,6 +2,8 @@ package com.frankding.android.ridetj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class catchrideActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class catchrideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catchride);
+
+        String[] ridesArray = {"option1","option2","option3","option4"};
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_catchride, ridesArray);
+
+        ListView listView = (ListView) findViewById(R.id.ridesList);
+        listView.setAdapter(adapter);
     }
+    
 }
